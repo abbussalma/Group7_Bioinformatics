6f45c15e426971efa3304d55263c4507aeca6edb
Mohammed rabiu anas <95101195+Anassana@users.noreply.github.com>
2021-11-30 23:28:15 +0100

    Add files via upload

---
+++ script_nass - Copy.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env
+console.log('Name: Mohammed Rabiu Anas');
+console.log('Email: anasanatomy@gmail.com');
+console.log('Area of interest: genomics');
+console.log('Slack ID: @mohammedrabiuanas');
