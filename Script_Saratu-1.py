87656828d1071db4cd46d6aaeddb487609f627ed
Saratushuaibu <95252167+Saratushuaibu@users.noreply.github.com>
2021-11-30 22:54:14 +0100

    Add files via upload

---
+++ Script_Saratu-1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#<-----------Saratu Project------------>
+print("\t-------------BIOINFORMATICS SCRIPT---------------\n")
+
+print("\t\tHello Saratu Shuaibu, below are your informations:\n")
+print("Name: " + "\t\t\t" + "Saratu Shuaibu")
+print("Email: " + "\t\t\t"+ "saratushuaibu2017@gmail.com")
+print("Slack_username: " + "\t" + "@Saratu_Shuaibu")
+print("Area of interest: " + "\t" + "Genomics")
