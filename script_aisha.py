48de36ca98def14bd1ab57961c599a134f308d3a
abbussalma <94013467+abbussalma@users.noreply.github.com>
2021-11-30 19:03:30 +0100

    Delete script_aisha.py

--- script_aisha.py
+++
@@ -1,9 +0,0 @@
-#<-----------Aisha Project------------>
-print("\t-------------BIOINFORMATICS SCRIPT---------------\n")
-
-print("\t\tHello Aisha Abdulsalam, below are your informations:\n")
-print("Name: " + "\t\t\t" + "Aisha Abdulsalam")
-print("Email: " + "\t\t\t"+ "aishaabdul13@gmail.com")
-print("Slack_username: " + "\t" + "@Aisha_Abdulsalam")
-print("Area of interest: " + "\t" + "Metagenomics")
-
