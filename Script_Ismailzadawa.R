64d2c6da20269a9bb1d537c31808c1b482df8cc5
Ismailaliyuzadawa <94997628+Ismailaliyuzadawa@users.noreply.github.com>
2021-12-01 14:37:39 +0100

    Add files via upload

---
+++ Script_Ismailzadawa.R
@@ -0,0 +1,13 @@
+#!/usr/bin/env Rscript
+
+name<- c('Ismail Aliyu Zadawa')
+name
+
+email<- c('iazadawa9009@gmail.com')
+email
+
+bioinformatics<- c('genomics')
+bioinformatics
+
+slack<- c('@Ismailaliyuzadawa')
+slack
