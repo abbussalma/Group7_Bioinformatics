839aa05e3ce6736190017684eb16aa85e95507e2
AbdulazeezYunusa0142 <95096062+AbdulazeezYunusa0142@users.noreply.github.com>
2021-12-01 15:39:34 +0100

    Add files via upload

---
+++ Script_Abdulazeez.R.R
@@ -0,0 +1,17 @@
+#!/usr/bin/env Rscript
+
+# FULL NAME
+name<- c('Abdulazeez Yunusa')
+name
+
+# EMAIL ADDRESS
+email<- c('docabdul1010@gmail.com')
+email
+
+# BIOINFORMATIC AREA OF INTEREST
+bioinformatic<- c('genomics')
+bioinformatic
+
+# SLACK USERNAME 
+slack<- c('@abdulazeezYunusa0142')
+slack
