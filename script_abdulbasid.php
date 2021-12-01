7ce1527a4ef4002f6d4c6b636e69bf500045c369
ajkalaa <95326908+ajkalaa@users.noreply.github.com>
2021-12-01 17:42:53 +0100

    Add files via upload

---
+++ script_abdulbasid.php
@@ -0,0 +1,11 @@
+#!/usr/bin/php
+
+
+<?php
+$Name = 'Abdulbasid Jibrin Muhd';
+$email = 'abdulbasidjibrinmuhd@gmail.com';
+$Slack = '@abdulbasid_jibrin_muhd';
+$Area = 'Genomics';
+
+echo "$name, $email, $slack, $area";
+?>
