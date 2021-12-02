8120046dfeaa4ff3bfa2ae864e990c2d60c21a0f
abbussalma <94013467+abbussalma@users.noreply.github.com>
2021-12-02 11:07:26 +0100

    Update Script_Abdulazeez.R.R

--- Script_Abdulazeez.R.R
+++ Script_Abdulazeez.R.R
@@ -1,17 +1,3 @@
-#!/usr/bin/env Rscript
+#!/usr/bin/env bash
 
-# FULL NAME
-name<- c('Abdulazeez Yunusa')
-name
-
-# EMAIL ADDRESS
-email<- c('docabdul1010@gmail.com')
-email
-
-# BIOINFORMATIC AREA OF INTEREST
-bioinformatic<- c('genomics')
-bioinformatic
-
-# SLACK USERNAME 
-slack<- c('@abdulazeezYunusa0142')
-slack
+echo "Abdulazeez Yunusa,docabdul1010@gmail.com,@abdulazeezYunusa0142"
