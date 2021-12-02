7cd8293c3a59649e61fd2f93cb9b1a01ebd4a518
abbussalma <94013467+abbussalma@users.noreply.github.com>
2021-12-02 11:38:54 +0100

    Update script_aisha.py

--- script_aisha.py
+++ script_aisha.py
@@ -1,9 +1,4 @@
-#<-----------Aisha Project------------>
-print("\t-------------BIOINFORMATICS SCRIPT---------------\n")
+#!/usr/bin/env bash
 
-print("\t\tHello Aisha Abdulsalam, below are your informations:\n")
-print("Name: " + "\t\t\t" + "Aisha Abdulsalam")
-print("Email: " + "\t\t\t"+ "aishaabdul13@gmail.com")
-print("Slack_username: " + "\t" + "@Aisha_Abdulsalam")
-print("Area of interest: " + "\t" + "Metagenomics")
+echo "Aisha Abdulsalam,aishaabdul13@gmail.com,@Aisha_Abdulsalam,Metagenomics"
 
