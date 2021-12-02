e579edd328b37a528afe9c3de944f6f91ae4334d
abbussalma <94013467+abbussalma@users.noreply.github.com>
2021-12-02 20:48:19 +0100

    Delete ismaeel.java

--- ismaeel.java
+++
@@ -1,8 +0,0 @@
-public class ismaeel {
-
- public static void main(String[] args) {
-  System.out.println("Name:Ismail Haladu Aliyu\nEmail: ismaeelkhaleedali@gmail.com\n Slack Username: @ismailhaladualiyu\n Area of Interest: Genomics");
- }
-
-}
-
