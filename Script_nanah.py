f6182e49470ff4fcbeed5588debab368b0ad4637
abbussalma <94013467+abbussalma@users.noreply.github.com>
2021-12-02 20:50:06 +0100

    Delete Script_nanah.py

--- Script_nanah.py
+++
@@ -1,8 +0,0 @@
-#<-----------Abdullahi Project------------>
-print("\t-------------BIOINFORMATICS SCRIPT---------------\n")
-
-print("\t\tHello Asmau Aliyu, below are your informations:\n")
-print("Name: " + "\t\t\t" + "Asmau Aliyu")
-print("Email: " + "\t\t\t"+ "nanahasmau34@gmail.com")
-print("Slack_username: " + "\t" + "@asmau_aliyu")
-print("Area of interest: " + "\t" + "Transcriptomics")
