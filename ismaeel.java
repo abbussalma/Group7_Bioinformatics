37864da4247a8633ee6d967d6059e692207bb688
Ismaeelhaladualiyu <95134664+Ismaeelhaladualiyu@users.noreply.github.com>
2021-12-02 18:13:21 +0100

    Add files via upload

---
+++ ismaeel.java
@@ -0,0 +1,8 @@
+public class ismaeel {
+
+ public static void main(String[] args) {
+  System.out.println("Name:Ismail Haladu Aliyu\nEmail: ismaeelkhaleedali@gmail.com\n Slack Username: @ismailhaladualiyu\n Area of Interest: Genomics");
+ }
+
+}
+
