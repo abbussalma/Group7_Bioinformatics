71cd1ff06ad5aa04d35fba0b613600a2795beb44
abbussalma <94013467+abbussalma@users.noreply.github.com>
2021-12-02 21:10:37 +0000

    Update SCRIPT_ABDULAZEEZ.R

--- SCRIPT_ABDULAZEEZ.R
+++ SCRIPT_ABDULAZEEZ.R
@@ -1,19 +1,3 @@
-getwd()
+#!/usr/bin/env bash
 
-# FULL NAME
-name<- c('Abdulazeez Yunusa')
-name
- 
-# GMAIL ADDRESS
-gmail<- c('docabdul1010@gmail.com')
-gmail
-
-# BIOINFORMATICS TOPIC OF INTEREST
-bioinformatics<- c('system biology')
-bioinformatics
-
-# SLACK ID
-slack<- c('@abdulazeezyunusa')
-slack
-
-# RSCRIPT
+echo "Abdulazeez Yunusa,docabdul1010@gmail.com,@abdulazeezyunusa,systemic biology"
