40dc9f272e84724ce7e9811a72c253581da05836
abbussalma <94013467+abbussalma@users.noreply.github.com>
2021-12-02 11:09:43 +0100

    Update Script_Ismailzadawa.R

--- Script_Ismailzadawa.R
+++ Script_Ismailzadawa.R
@@ -1,13 +1,4 @@
-#!/usr/bin/env Rscript
+#!/usr/bin/env bash
 
-name<- c('Ismail Aliyu Zadawa')
-name
+echo "Ismail Aliyu Zadawa,iazadawa9009@gmail.com,@Ismailaliyuzadawa,genomics"
 
-email<- c('iazadawa9009@gmail.com')
-email
-
-bioinformatics<- c('genomics')
-bioinformatics
-
-slack<- c('@Ismailaliyuzadawa')
-slack
