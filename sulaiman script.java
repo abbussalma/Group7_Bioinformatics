1ed5882877958d547a419041b8f28d97ead3c155
abbussalma <94013467+abbussalma@users.noreply.github.com>
2021-12-02 16:55:52 +0100

    Delete sulaiman script.java

--- sulaiman script.java
+++
@@ -1,8 +0,0 @@
-public class sulaiman {
-
- public static void main(String[] args) {
-  System.out.println("Name:Sulaiman Ahmad\n Email: sulaimanahmadgmw@gmail.com\n Slack Username: @sulaimanahmad\n Area of Interest: System in Biology");
- }
-
-}
-
