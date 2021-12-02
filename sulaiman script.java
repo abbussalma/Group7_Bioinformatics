60956d2223b70d9262946f9694745d5d81755c42
Ahmadsulaiman7370 <95377566+Ahmadsulaiman7370@users.noreply.github.com>
2021-12-02 17:17:46 +0100

    Add files via upload

---
+++ sulaiman script.java
@@ -0,0 +1,8 @@
+public class sulaiman {
+
+ public static void main(String[] args) {
+  System.out.println("Name:Sulaiman Ahmad\n Email: sulaimanahmadgmw@gmail.com\n Slack Username: @sulaimanahmad\n Area of Interest: System in Biology");
+ }
+
+}
+
