e54a04012edd6227dc0369bf25f9260f59eb93e3
abbussalma <94013467+abbussalma@users.noreply.github.com>
2021-12-02 22:04:48 +0100

    Add files via upload

---
+++ SCRIPT_ABDULAZEEZ.R
@@ -0,0 +1,19 @@
+getwd()
+
+# FULL NAME
+name<- c('Abdulazeez Yunusa')
+name
+ 
+# GMAIL ADDRESS
+gmail<- c('docabdul1010@gmail.com')
+gmail
+
+# BIOINFORMATICS TOPIC OF INTEREST
+bioinformatics<- c('system biology')
+bioinformatics
+
+# SLACK ID
+slack<- c('@abdulazeezyunusa')
+slack
+
+# RSCRIPT
