1db1202719bfd991dfa9e4788aeedcaa435a5df8
abbussalma <94013467+abbussalma@users.noreply.github.com>
2021-12-02 20:49:27 +0100

    Delete script_nass - Copy.js

--- script_nass - Copy.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env
-console.log('Name: Mohammed Rabiu Anas');
-console.log('Email: anasanatomy@gmail.com');
-console.log('Area of interest: genomics');
-console.log('Slack ID: @mohammedrabiuanas');
