31b99f029905ec58223da1798dd5eb04ff0dbf35
Ismailaliyuzadawa <94997628+Ismailaliyuzadawa@users.noreply.github.com>
2021-12-02 17:21:38 +0100

    Add files via upload

---
+++ Script_Ismail.R
@@ -0,0 +1,18 @@
+# NAME
+name<- c("Ismail Aliyu Zadawa")
+name
+
+# EMAIL ADDRESS
+email<- c("iazadawa9009@gmail.com")
+email
+
+# SLACK USERNAME
+slack<- c("@ismailaliyuzadawa")
+slack
+
+# BIOINFORMATIC AREA OF INTEREST
+bioinformatic<- c("genomics")
+bioinformatic
+
+
+
