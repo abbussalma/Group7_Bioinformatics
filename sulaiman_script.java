253c597db5b403faaa6d19d7660e339f00e5641c
abbussalma <94013467+abbussalma@users.noreply.github.com>
2021-12-02 12:02:59 +0100

    Update sulaiman_script.java

--- sulaiman_script.java
+++ sulaiman_script.java
@@ -1,12 +1,3 @@
-#!/usr/local/bin/node
-// this script prints my personal detail
+#!/usr/bin/env bash
 
-// assigning some variables of my personal information
-var Name = "Sulaiman Ahmad";
-var Email = "sulaimanahmadgmw@gmail.com";
-var Slack_Username = "@sulaimanahmad"; 
-var Area_of_Interest: "System in Biology";
- 
-
-console.log (name +', ' + email + ', '+ slack_id +', '+ area);
-
+echo "Sulaiman Ahmad,sulaimanahmadgmw@gmail.com,@sulaimanahmad,System in Biology"
